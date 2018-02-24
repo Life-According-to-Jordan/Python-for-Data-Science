f5332de6d5c00df192de61f230e3aa344fbac557
Jordan Hoehne <33847459+Life-According-to-Jordan@users.noreply.github.com>
2018-02-24 15:45:10 -0600

    Update Classifier_Tree.py

--- Classifier_Tree.py
+++ Classifier_Tree.py
@@ -1,13 +1,15 @@
 #import dependencies 
+#import scikit and specify the module we want to import
 from sklearn import tree
 
+#clf as Decision Tree 
 clf = tree.DecisionTreeClassifier()
 
-# Here is some sample data 
+#X is a list of lists (a text string)
 #[height, weight, shoe_size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
-     [190, 90, 47], [175, 64, 39],
-     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+     [190, 90, 47], [175, 64, 39], [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
 #labeled data 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
      'female', 'male', 'male']
@@ -16,8 +18,8 @@ Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
 #fit/trains the model on our sample data 
 clf = clf.fit(X, Y)
 
-#predict whether male or female given the parameters 
+#store our prediction to determine whether the parameters entered are either male or female. 
 prediction = clf.predict([[190, 70, 43]])
 
-#tells python to classify the parameters given above 
+#print our prediction
 print(prediction)
