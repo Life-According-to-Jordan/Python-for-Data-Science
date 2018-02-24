bec3c4801159e73953919cd5251cc6fc4f8d1a3e
Jordan Hoehne <33847459+Life-According-to-Jordan@users.noreply.github.com>
2018-02-24 16:26:07 -0600

    Update Twitter_Sentiment_Analysis.py
    Adding comments, intuition, and logic.

--- Twitter_Sentiment_Analysis.py
+++ Twitter_Sentiment_Analysis.py
@@ -1,32 +1,51 @@
-#	1. Register for Twitter API
-#	2. Install Dependencies
-#   Write our Script
+'''
+BEFORE YOU START
+Register for Twitter API: https://apps.twitter.com/
+Install Dependencies:
+    [pip install tweepy]
+    [pip install textblob]
+'''
 
 #Used to access twitter API
 import tweepy
+
 #Used to perform sentiment analysis
 from textblob import TextBlob
 
+#Twitter Authentication 
 consumer_key = 'YOUR CONSUMER KEY'
 consumer_secret = 'YOUR CONSUMER SECRET'
-
 access_token = 'YOUR ACCESS TOKEN'
 access_token_secret = 'YOUR ACCESS TOKEN SECRET'
 
+#OAuth authentication 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
+#main variable from which we will work, single authentication arguement 
 api = tweepy.API(auth)
 
 #search for tweets
+public_tweets = api.search('SEARCH KEY WORD HERE')
 
-public_tweets = api.search('Kitten')
-
+#method retrieves tweets to terminal 
+#for loop in a list
 for tweet in public_tweets:
+    #string version of tweet
     print(tweet.text)
-    analysis = TextBlob(tweet.text)
-    #this returns the sentiment of the tweet
+    #analysis of each tweet for the loop
     #polarity measures how positive or negative it is
     #subjectivity measures the degree of opinion or fact
+    analysis = TextBlob(tweet.text)
+    #this returns the sentiment of the tweet
     print(analysis.sentiment)
     print("")
+
+'''
+HELPFUL DEFINITIONS FOR SENTIMENT ANALYSIS
+Natural Language Processing (NLP) - Branch of CS and AI that helps computers interpret human language.
+Sentiment Analysis - computational analysis of text to determine the writter's attitude 
+Tokenization - divides text into individual words 
+Bag of Words (BOW) - method of extracting features from text
+API - Application Programming Interface 
+'''
